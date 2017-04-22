643f547b6b815488e2d4221d8deaa20243b7aaba
Craig Reynolds <craig@wyldphyre.com>
2017-04-22 20:31:28 +0800

    Fix laucher and task config after earlier restructuing

--- app/Program.cs
+++ app/Program.cs
@@ -9,16 +9,15 @@ namespace app
     static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");
+      Console.WriteLine("Nothing else implemented yet!");
 
-      // var Operation = new Operation();
-      // Operation.Testy();
       // To Do: Start by taking 2 arguments. The first is either a file or folder to process. 
       // The second parameter is the instruction file of what to look for
-      if (args.Any())
-      {
-        var filename = args.First();
-        Console.WriteLine(filename);
-      }
+      // if (args.Any())
+      // {
+      //   var filename = args.First();
+      //   Console.WriteLine(filename);
+      // }
     }
   }
 }
